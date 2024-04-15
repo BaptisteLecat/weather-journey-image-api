892cc53e5d06ad8d75843fb0f06b0150b1e8d40b
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2024-04-15 10:50:12 +0200

    Setup integration test
    - Add first tests for the user module
    - Setup coverage reports

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { User } from '../src/modules/users/entities/user.entity';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,56 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('User Module', () => {
+    describe('GET /users (GET)', () => {
+      it('/users/:id (GET) : should return an user', () => {
+        return request(app.getHttpServer())
+          .get('/users/Ixt0stWixWMX1dLHKLpw1AixiAWZ')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect((res) => {
+            // Vérification de la structure et des types de l'objet utilisateur
+            expect(res.body).toEqual(expect.objectContaining({
+              email: expect.any(String),
+              frequencies: expect.any(Array),
+              id: expect.any(String),
+              styles: expect.any(Array),
+            }));
+          });
+      });
+
+      it('/users/:id (GET) : should return an error if user not found', () => {
+        return request(app.getHttpServer())
+          .get('/users/unknown')
+          .expect(404)
+          .expect('Content-Type', /json/)
+          .expect((res) => {
+            // Vérification de la structure et des types de l'objet erreur
+            expect(res.body).toEqual(expect.objectContaining({
+              statusCode: expect.any(Number),
+              message: expect.any(String),
+            }));
+          });
+      });
+    });
+    describe('GET /users (GET)', () => {
+      it('/users (GET) : should return an array of users', () => {
+        return request(app.getHttpServer())
+          .get('/users')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect((res) => {
+            // Vérification de la structure et des types de l'objet utilisateur
+            expect(res.body).toEqual(expect.arrayContaining([
+              expect.objectContaining({
+                email: expect.any(String),
+                frequencies: expect.any(Array),
+                id: expect.any(String),
+                styles: expect.any(Array),
+              }),
+            ]));
+          });
+      });
+    });
   });
 });

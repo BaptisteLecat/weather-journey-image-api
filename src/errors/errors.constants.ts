c69c45b0c5f20c7ed9360aeba18641a4242bf5f3
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2024-04-15 15:56:08 +0200

    Complete E2E Tests
    - Adding test for generations
    - Improve test structure
    - Test the authentication

--- src/errors/errors.constants.ts
+++ src/errors/errors.constants.ts
@@ -3,26 +3,26 @@ export const USER_EMAIL_NOT_FOUND = "User email not found";
 export const USER_ID_NOT_FOUND = "User id not found";
 export const API_KEY_NOT_FOUND = {
     message: "API key not found",
-    code: "404000"
+    statusCode: 404000
 };
 export const TOKEN_NOT_FOUND = {
     message: "Token not found",
-    code: "404001"
+    statusCode: 404001
 };
 export const TOKEN_MALFORMED = {
     message: "Token malformed",
-    code: "404002"
+    statusCode: 404002
 };
 
 export const INVALID_API_KEY = {
     message: "Invalid API key",
-    code: "401000"
+    statusCode: 401000
 };
 export const INVALID_TOKEN = {
     message: "Invalid token",
-    code: "401001"
+    statusCode: 401001
 };
 export const EXPIRED_TOKEN = {
     message: "Expired token",
-    code: "401002"
+    statusCode: 401002
 };
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,9 +3,35 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { User } from '../src/modules/users/entities/user.entity';
+import axios from 'axios';
 
-describe('AppController (e2e)', () => {
+function authenticateUser() {
+  return axios.post('http://127.0.0.1:9099/identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=key', { "email": "alice@gmail.com", "password": "alicealice", "returnSecureToken": true }).then((response) => response.data.idToken);
+}
+
+describe('API (e2e)', function () {
   let app: INestApplication;
+  let user: User;
+  let token: string;
+  let expiredToken: string;
+
+  beforeAll(async () => {
+    user = new User(
+      'Ixt0stWixWMX1dLHKLpw1AixiAWZ',
+      'Alice',
+      'Alice',
+      'alice@gmail.com',
+      [],
+      [],
+    );
+
+    token = await authenticateUser();
+    expiredToken = "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJuYW1lIjoiYWxpY2UiLCJwaWN0dXJlIjoiIiwiZW1haWwiOiJhbGljZUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImF1dGhfdGltZSI6MTcxMzE3NzAyOCwidXNlcl9pZCI6IjNLOWZZemdKR2xJeDRPdVFnZm1VMVlFRG4ybGIiLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImFsaWNlQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn0sImlhdCI6MTcxMzE3NzAyOCwiZXhwIjoxNzEzMTgwNjI4LCJhdWQiOiJ3ZWF0aGVyYXBwLWpvdXJuZXkiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vd2VhdGhlcmFwcC1qb3VybmV5Iiwic3ViIjoiM0s5Zll6Z0pHbEl4NE91UWdmbVUxWUVEbjJsYiJ9.";
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,14 +39,15 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
     await app.init();
   });
 
-  describe('User Module', () => {
-    describe('GET /users (GET)', () => {
-      it('/users/:id (GET) : should return an user', () => {
-        return request(app.getHttpServer())
-          .get('/users/Ixt0stWixWMX1dLHKLpw1AixiAWZ')
+  describe('User Module', function () {
+    describe('/users/:id (GET) :', function () {
+      it('should return an user', async function () {
+        return await request(app.getHttpServer())
+          .get(`/users/${user.id}`)
           .expect(200)
           .expect('Content-Type', /json/)
           .expect((res) => {
@@ -34,8 +61,8 @@ describe('AppController (e2e)', () => {
           });
       });
 
-      it('/users/:id (GET) : should return an error if user not found', () => {
-        return request(app.getHttpServer())
+      it('should return an error if user not found', async function () {
+        return await request(app.getHttpServer())
           .get('/users/unknown')
           .expect(404)
           .expect('Content-Type', /json/)
@@ -48,9 +75,9 @@ describe('AppController (e2e)', () => {
           });
       });
     });
-    describe('GET /users (GET)', () => {
-      it('/users (GET) : should return an array of users', () => {
-        return request(app.getHttpServer())
+    describe('/users (GET)', function () {
+      it('should return an array of users', async function () {
+        return await request(app.getHttpServer())
           .get('/users')
           .expect(200)
           .expect('Content-Type', /json/)
@@ -65,6 +92,138 @@ describe('AppController (e2e)', () => {
               }),
             ]));
           });
+      });
+    });
+  });
+
+  describe('Generation Module', function () {
+    describe('/generations (POST)', function () {
+      it('should return an array of generations', async function () {
+        const res = await request(app.getHttpServer())
+          .post('/locations/0eWSjfdXTUT91QTWYR5g/generations')
+          .set("api-key", "api_key")
+          .set("Authorization", `Bearer ${token}`)
+          .set("Content-Type", "application/json")
+          .send({ "time": "Alice", "weather": "Alice" })
+          .expect(201)
+          .expect('Content-Type', /json/)
+          .expect((res) => {
+            // Vérification de la structure et des types de l'objet génération
+            expect(res.body).toEqual(expect.objectContaining({
+              id: expect.any(String),
+              generatedImage: null,
+              progress: expect.any(Number),
+              prompt: expect.any(String)
+            }));
+          });
+      }, 20000);
+
+      it('should return an error if location not found', async function () {
+        return await request(app.getHttpServer())
+          .post('/locations/unknown/generations')
+          .set("api-key", "api_key")
+          .set("Authorization", `Bearer ${token}`)
+          .set("Content-Type", "application/json")
+          .send({ "time": "Alice", "weather": "Alice" })
+          .expect(404)
+          .expect('Content-Type', /json/)
+          .expect((res) => {
+            // Vérification de la structure et des types de l'objet erreur
+            expect(res.body).toEqual(expect.objectContaining({
+              statusCode: expect.any(Number),
+              message: expect.any(String),
+            }));
+          });
+      });
+
+      describe('Authentication', function () {
+        describe('Authentication Token', function () {
+          it('should return an error if user not authenticated', async function () {
+            return await request(app.getHttpServer())
+              .post('/locations/0eWSjfdXTUT91QTWYR5g/generations')
+              .set("api-key", "api_key")
+              .expect(400)
+              .expect('Content-Type', /json/)
+              .expect((res) => {
+                // Vérification de la structure et des types de l'objet erreur
+                expect(res.body).toEqual(expect.objectContaining({
+                  statusCode: expect.any(Number),
+                  message: expect.any(String),
+                }));
+              });
+          });
+
+          it('should return an error if user token is expired', async function () {
+            return await request(app.getHttpServer())
+              .post('/locations/0eWSjfdXTUT91QTWYR5g/generations')
+              .set("api-key", "api_key")
+              .set("Authorization", `Bearer ${expiredToken}`)
+              .set("Content-Type", "application/json")
+              .send({ "time": "Alice", "weather": "Alice" })
+              .expect(401)
+              .expect('Content-Type', /json/)
+              .expect((res) => {
+                // Vérification de la structure et des types de l'objet erreur
+                expect(res.body).toEqual(expect.objectContaining({
+                  statusCode: expect.any(Number),
+                  message: expect.any(String),
+                }));
+              });
+          });
+
+          it('should return an error if user token is invalid', async function () {
+            return await request(app.getHttpServer())
+              .post('/locations/0eWSjfdXTUT91QTWYR5g/generations')
+              .set("api-key", "api_key")
+              .set("Authorization", `Bearer invalid_token`)
+              .set("Content-Type", "application/json")
+              .send({ "time": "Alice", "weather": "Alice" })
+              .expect(400)
+              .expect('Content-Type', /json/)
+              .expect((res) => {
+                // Vérification de la structure et des types de l'objet erreur
+                expect(res.body).toEqual(expect.objectContaining({
+                  statusCode: expect.any(Number),
+                  message: expect.any(String),
+                }));
+              });
+          });
+        });
+        describe('Authentication API Key', function () {
+          it('should return an error if user not authenticated', async function () {
+            return await request(app.getHttpServer())
+              .post('/locations/0eWSjfdXTUT91QTWYR5g/generations')
+              .set("Authorization", `Bearer ${token}`)
+              .set("Content-Type", "application/json")
+              .expect(400)
+              .expect('Content-Type', /json/)
+              .expect((res) => {
+                // Vérification de la structure et des types de l'objet erreur
+                expect(res.body).toEqual(expect.objectContaining({
+                  statusCode: expect.any(Number),
+                  message: expect.any(String),
+                }));
+              });
+          });
+
+          it('should return an error if user API Key is invalid', async function () {
+            return await request(app.getHttpServer())
+              .post('/locations/0eWSjfdXTUT91QTWYR5g/generations')
+              .set("api-key", "invalid_api_key")
+              .set("Authorization", `Bearer ${token}`)
+              .set("Content-Type", "application/json")
+              .send({ "time": "Alice", "weather": "Alice" })
+              .expect(401)
+              .expect('Content-Type', /json/)
+              .expect((res) => {
+                // Vérification de la structure et des types de l'objet erreur
+                expect(res.body).toEqual(expect.objectContaining({
+                  statusCode: expect.any(Number),
+                  message: expect.any(String),
+                }));
+              });
+          });
+        });
       });
     });
   });
